2516495834bc3daec68bebe2a29dce5d6435a2f1
Dong <Zdong3119@conestogac.on.ca>
2025-04-01 21:24:25 -0400

    Got posts objects with Ajax

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,1 +1,34 @@
-console.log('hello world')
+// console.log('hello world');
+
+const helloWorldBox = document.getElementById('hello_world');
+const postsBox = document.getElementById('posts_box');
+
+$.ajax({
+    type: 'GET',
+    url:'hello-world',
+    success: function(response) {
+        console.log('success',response.text);
+        helloWorldBox.textContent = response.text;
+    },
+    error: function(error) {
+        console.log('error',error);
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url:'data',
+    success: function(response) {
+        console.log('success', response);
+        const data = response.data;
+        console.log('data', data);
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b></br>
+            `
+        })
+    },
+    error: function(error) {
+        console.log('error',error);
+    }
+})
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
+    hello_world_view,
+    load_post_data_view
 )
 app_name = 'posts' 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+    path('hello-world/', hello_world_view, name='hello-world'),
+    path('data/', load_post_data_view, name='post-data'),
 ]
 
