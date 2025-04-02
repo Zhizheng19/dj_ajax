afbc45d05aa01ce1b096e247cdef7916ae88330a
Dong <Zdong3119@conestogac.on.ca>
2025-04-02 00:05:21 -0400

    Added Ajax call for like button

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -6,6 +6,51 @@ const spinnerBox = document.getElementById('spinner-box');
 const loadBtn = document.getElementById('load-btn');
 const endBox = document.getElementById('end-box');
 
+const getCookie = (name) => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+const csrftoken = getCookie('csrftoken');
+
+const likeUnlikePosts = (e) => {
+    const likeUnlikeForms = [...document.getElementsByClassName('like-unlike-forms')];
+    likeUnlikeForms.forEach(form => {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const clickedId = e.target.getAttribute('data-form-id');
+            console.log('clickedId', clickedId);
+            const clickedBtn = document.getElementById(`like-unlike-${clickedId}`);
+
+            $.ajax({
+                type: 'POST',
+                url: '/like-unlike/',
+                data: {
+                    'csrfmiddlewaretoken': csrftoken,   
+                    'pk': clickedId,
+                },
+                success: function (response) {
+                    console.log('success', response);
+                    clickedBtn.textContent = response.liked ? `Unlike (${response.count})` : `Like (${response.count})`
+                },
+                error: function (error) {
+                    console.log('error', error);
+                }
+            })
+        })
+    })
+}
+
 let visible = 3;    // how many posts to show
 const getData = () => {
     $.ajax({
@@ -13,7 +58,7 @@ const getData = () => {
         url: `data/${visible}`,
         success: function (response) {
             console.log('success', response);
-            const data = response.data;
+            const data = response.data; // get all the posts data
             console.log('data', data);
             setTimeout(() => {
                 spinnerBox.classList.add('not-visible');
@@ -26,18 +71,23 @@ const getData = () => {
                     </div>
                     <div class="card-footer">  
                         <div class="row">
-                            <div class="col-1">                                 
+                            <div class="col-2">                                 
                                 <a href="#" class="btn btn-primary">Details</a>
                             </div>
-                            <div class="col-1">                                 
-                                <a href="#" class="btn btn-primary">like</a>
+                            <div class="col-2">          
+                                <form class="like-unlike-forms" data-form-id="${el.id}">                       
+                                    <button href="#" class="btn btn-primary" id="like-unlike-${el.id}">
+                                        ${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div> 
 
                 </div>
                 `
-                })
+                });
+                likeUnlikePosts();
             }, 100)
             console.log(response.size);
             if (response.size === 0) {
