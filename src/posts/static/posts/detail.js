56593bfdda3cadce7db38e0b05419f70e6418086
Dong <Zdong3119@conestogac.on.ca>
2025-04-07 16:16:17 -0400

    Requested data from the detail page

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -2,7 +2,23 @@ console.log('hello detail page');
 const backBtn = document.getElementById('back-btn');
 const updateBtn = document.getElementById('update-btn');
 const deleteBtn = document.getElementById('delete-btn');
+const spinnerBox = document.getElementById('spinner-box');
+const url = window.location.href + 'data/';
 
 backBtn.addEventListener('click', (e) => {
     history.back();
+});
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function (response) {
+        console.log('success', response);
+        spinnerBox.classList.add('not-visible');
+        // const data = response.data;
+        // console.log('data', data);
+    },
+    error: function (error) {
+        console.log('error', error);
+    }
 });
