9fa02de1761a795f87a8bae2a196ec1d07f8c3ca
Dong <Zdong3119@conestogac.on.ca>
2025-04-07 19:34:58 -0400

    Hide the update and delete button in detail page if the user is not the author of the post

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,4 +1,5 @@
 console.log('hello detail page');
+const postBox = document.getElementById('post-box');
 const backBtn = document.getElementById('back-btn');
 const updateBtn = document.getElementById('update-btn');
 const deleteBtn = document.getElementById('delete-btn');
@@ -14,8 +15,16 @@ $.ajax({
     url: url,
     success: function (response) {
         console.log('success', response);
+        const data = response.data;
+        if (data.logged_in !== data.author ) {
+            console.log("different user");
+        } else {
+            console.log("same user");
+            updateBtn.classList.remove('not-visible');
+            deleteBtn.classList.remove('not-visible');
+        }
+
         spinnerBox.classList.add('not-visible');
-        // const data = response.data;
         // console.log('data', data);
     },
     error: function (error) {
